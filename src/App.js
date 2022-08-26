6463f19add0da8bb88e28b0a1e3d56f97cb98343
PriyankeshGithub <priyankeshshivam@gmail.com>
2022-08-27 01:56:57 +0530

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureDetails from './components/TemperatureDetails';
+import Forecast from './components/Forecast';
+import getWeatherData from './services/weatherService';
+import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react';
 function App() {
+  
+const {query, setQuery} = useState({q: 'berlin'})
+const [units, setUnits] = useState('metric')
+const [weather, setWeather] = useState(null) 
+
+useEffect(()=>{
+  const fetchWeather = async () => {
+  await getFormattedWeatherData({...query, units}).then((data)=>{
+    setWeather(data)
+  });
+  }
+  fetchWeather();
+
+},[query, units])
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <TopButtons setQuery={setQuery} />
+    <Inputs />
+
+    {weather &&(
+      <div>
+      <TimeAndLocation weather={weather}/>
+    <TemperatureDetails weather={weather}/>
+    <Forecast title="Hourly forecast"/>
+    <Forecast title="Hourly forecast"/>
+    </div>
+    )}
+    
     </div>
   );
 }
